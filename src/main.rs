c6eecc799f975ef72afba362bedbf612c29ca3eb
J2ghz <j2.00ghz@gmail.com>
2020-08-24 00:16:37 +0200

    simple output

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,13 @@
+use image::{ImageBuffer, ImageFormat, RgbImage};
+
 fn main() {
     println!("Hello, world!");
+    let mut img: RgbImage = ImageBuffer::new(200, 100);
+    for x in 0..200 {
+        for y in 0..100 {
+            img.put_pixel(x, y, [x as u8, 200 - (y * 2) as u8, 0].into());
+        }
+    }
+    img.save_with_format("out.bmp", ImageFormat::Bmp)
+        .expect("Couldn't save image");
 }
